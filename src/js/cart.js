dd249b583751d57fc162e4afb8b5c3176b82a1d7
alansanchezzamora <122119972+alansanchezzamora@users.noreply.github.com>
2024-04-27 00:18:30 -0600

    added renderTotal function - backlog1

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,13 +1,37 @@
 import { getLocalStorage } from "./utils.mjs";
 
+//This function return true if the Local Storage has items
+function cartHasItems() {
+  if (getLocalStorage("so-cart") != null) {
+    return true;
+  }
+}
+
+//Function reads calculates the final price of the items in the cart, backlog1
+function renderTotal() {
+  const cartItems = getLocalStorage("so-cart");
+  let total = 0;
+  if (cartHasItems()) {
+    for (let i = 0; i < cartItems.length; i++) {
+      total += JSON.parse(cartItems[i].FinalPrice);
+    }
+  }
+  document.querySelector(".cart-total").innerHTML += total;
+}
+
+//Function to render cart content, backlog1
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItemTemplate(cartItems);
-  document.querySelector(".product-list").innerHTML = htmlItems;
+  if (cartHasItems()) {
+    for (let i = 0; i < cartItems.length; i++) {
+      const htmlItems = cartItemTemplate(cartItems[i]);
+      document.querySelector(".product-list").innerHTML += htmlItems;
+    }
+  }
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
+  let newItem = `<li class="cart-card divider">
     <a href="#" class="cart-card__image">
       <img
         src="${item.Image}"
@@ -21,8 +45,8 @@ function cartItemTemplate(item) {
     <p class="cart-card__quantity">qty: 1</p>
     <p class="cart-card__price">$${item.FinalPrice}</p>
   </li>`;
-
   return newItem;
 }
 
 renderCartContents();
+renderTotal();
