cf1fccf4bfc422f420c6e1ab17e6cd57bb5b2a72
alansanchezzamora <122119972+alansanchezzamora@users.noreply.github.com>
2024-04-26 20:32:16 -0600

    individual act -alan

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
@@ -6,6 +6,9 @@ const dataSource = new ProductData("tents");
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
 }
+
+console.log(getLocalStorage());
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
