9e26c24ec45a8dc7f7780a97fcdb23d9dd8858a3
alansanchezzamora <122119972+alansanchezzamora@users.noreply.github.com>
2024-05-02 16:01:01 -0600

    deleted double button

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,23 +1,5 @@
 import { setLocalStorage } from "./utils.mjs";
 
-function productDetailsTemplate(product) {
-  return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
-    <h2 class="divider">${product.NameWithoutBrand}</h2>
-    <img
-      class="divider"
-      src="${product.Image}"
-      alt="${product.NameWithoutBrand}"
-    />
-    <p class="product-card__price">$${product.FinalPrice}</p>
-    <p class="product__color">${product.Colors[0].ColorName}</p>
-    <p class="product__description">
-    ${product.DescriptionHtmlSimple}
-    </p>
-    <div class="product-detail__add">
-      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-    </div></section>`;
-}
-
 export default class ProductDetails {
   constructor(productId, dataSource) {
     this.productId = productId;
@@ -46,3 +28,22 @@ export default class ProductDetails {
     );
   }
 }
+
+function productDetailsTemplate(product) {
+  return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+    <h2 class="divider">${product.NameWithoutBrand}</h2>
+    <img
+      class="divider"
+      src="${product.Image}"
+      alt="${product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">$${product.FinalPrice}</p>
+    <p class="product__color">${product.Colors[0].ColorName}</p>
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+    </div></section>`;
+}
+
