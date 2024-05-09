2dd8e97ed2292f8e426cf375f06cbf50230c7f25
Jose Quemba <131818747+josequemba@users.noreply.github.com>
2024-05-09 04:01:08 +0100

    Delete src directory

--- src/js/main.js
+++
@@ -1,4 +0,0 @@
-import Alert from "./Alert.mjs"; // Assuming alert.js is the file containing the alert class
-
-const alertInstance = new Alert("../alerts.json"); // Path to your JSON file
-alertInstance.readAlert("message"); // Specify the key name here
