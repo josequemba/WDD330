ac0e97f511b5e0a7f2a8f6af262f279218194355
josequemba <joseeliud33@gmail.com>
2024-05-13 14:41:57 +0100

    eliud

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,15 +1,27 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
-import { loadHeaderFooter, getParams, capitalizeFirstLetter } from './utils.mjs';
+import {
+  loadHeaderFooter,
+  getParams,
+  capitalizeFirstLetter,
+} from "./utils.mjs";
 
 loadHeaderFooter();
 
-const category = getParams('category');
+const category = getParams("category");
 const dataSource = new ProductData();
+
 document.querySelector(".products-title").innerHTML = capitalizeFirstLetter(category);
+
 const element = document.querySelector(".product-list");
 const searchForm = document.querySelector("#search-form");
 const searchResult = document.querySelector("#result-search");
 
-const listElement = new ProductListing(dataSource, category, element, searchForm, searchResult);
+const listElement = new ProductListing(
+  dataSource,
+  category,
+  element,
+  searchForm,
+  searchResult,
+);
 listElement.init();
